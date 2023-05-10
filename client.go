77fce17eda05497ada0aab0b8f862d7b48526b4f
Mmx233 <admin@mmxblog.com>
2023-05-10 18:31:50 +0800

    feat: 尝试支持新版 GeniusAuth

--- client.go
+++ client.go
@@ -4,19 +4,26 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/Mmx233/tool"
+	"github.com/ncuhome/GeniusAuthoritarianClient/pkg/signature"
 	"net/http"
+	"time"
 )
 
-func NewClient(domain string, httpClient *http.Client) *Client {
+func NewClient(domain, appCode, appSecret string, httpClient *http.Client) *Client {
 	return &Client{
-		Http:   tool.NewHttpTool(httpClient),
-		Domain: domain,
+		Http:      tool.NewHttpTool(httpClient),
+		Domain:    domain,
+		appCode:   appCode,
+		appSecret: appSecret,
 	}
 }
 
 type Client struct {
 	Http   *tool.Http
 	Domain string
+
+	appCode   string
+	appSecret string
 }
 
 func (c Client) Request(Type string, opt *tool.DoHttpReq) (*http.Response, error) {
@@ -25,14 +32,30 @@ func (c Client) Request(Type string, opt *tool.DoHttpReq) (*http.Response, error
 }
 
 type RequestVerifyToken struct {
-	Token  string   `json:"token"`
-	Groups []string `json:"groups,omitempty"`
+	Token string `json:"token"`
+}
+type reqVerifyToken struct {
+	RequestVerifyToken
+	AppCode   string `json:"appCode"`
+	TimeStamp int64  `json:"timeStamp"`
+	Signature string `json:"signature"`
 }
 
-func (c Client) VerifyToken(req *RequestVerifyToken) (*VerifyTokenResponse, error) {
+func (c Client) VerifyToken(req RequestVerifyToken) (*VerifyTokenResponse, error) {
+	var body = reqVerifyToken{
+		RequestVerifyToken: req,
+		AppCode:            c.appCode,
+		TimeStamp:          time.Now().Unix(),
+	}
+	body.Signature = signature.Gen(&signature.VerifyClaims{
+		Token:     body.Token,
+		TimeStamp: body.TimeStamp,
+		AppCode:   c.appCode,
+		AppSecret: c.appSecret,
+	})
 	res, e := c.Request("POST", &tool.DoHttpReq{
 		Url:  "v1/public/login/verify",
-		Body: req,
+		Body: &body,
 	})
 	if e != nil {
 		return nil, e
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ncuhome/GeniusAuthoritarianClient
 
 go 1.20
 
-require github.com/Mmx233/tool v0.6.9
+require github.com/Mmx233/tool v0.7.1
